b88314debc0251a8f6f5fe27916076166c379630
epfarmer <epfarmer@cougarnet.uh.edu>
2022-09-11 13:05:00 -0500

    eslint cleanup on backend

--- backend/models/models.js
+++ backend/models/models.js
@@ -1,95 +1,95 @@
-const uuid = require('uuid');
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const uuid = require('uuid')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-//collection for intakeData
-let primaryDataSchema = new Schema({
-    _id: { type: String, default: uuid.v1 },
-    firstName: {
-        type: String,
-        require: true
+// collection for intakeData
+const primaryDataSchema = new Schema({
+  _id: { type: String, default: uuid.v1 },
+  firstName: {
+    type: String,
+    required: true
+  },
+  middleName: {
+    type: String
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String
+  },
+  phoneNumbers: {
+    type: Array,
+    required: true
+  },
+  address: {
+    line1: {
+      type: String
     },
-    middleName: {
-        type: String,
+    line2: {
+      type: String
     },
-    lastName: {
-        type: String,
-        required: true
+    city: {
+      type: String,
+      required: true
     },
-    email: {
-        type: String
+    county: {
+      type: String
     },
-    phoneNumbers: {
-        type: Array,
-        required: true
-    },
-    address: {
-        line1: {
-            type: String
-        },
-        line2: {
-            type: String,
-        },
-        city: {
-            type: String,
-            required: true
-        },
-        county: {
-            type: String,
-        },
-        zip: {
-            type: String,
-        }
+    zip: {
+      type: String
     }
+  }
 }, {
-    collection: 'primaryData',
-    timestamps: true
-});
+  collection: 'primaryData',
+  timestamps: true
+})
 
-//collection for eventData
-let eventDataSchema = new Schema({
-    _id: { type: String, default: uuid.v1 },
-    eventName: {
-        type: String,
-        require: true
+// collection for eventData
+const eventDataSchema = new Schema({
+  _id: { type: String, default: uuid.v1 },
+  eventName: {
+    type: String,
+    required: true
+  },
+  services: {
+    type: Array
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  address: {
+    line1: {
+      type: String
     },
-    services: {
-        type: Array
+    line2: {
+      type: String
     },
-    date: {
-        type: Date,
-        required: true
+    city: {
+      type: String
     },
-    address: {
-        line1: {
-            type: String
-        },
-        line2: {
-            type: String,
-        },
-        city: {
-            type: String,
-        },
-        county: {
-            type: String,
-        },
-        zip: {
-            type: String,
-        }
+    county: {
+      type: String
     },
-    description: {
-        type: String,
-    },
-    attendees: [{
-        type: String
-    }]
+    zip: {
+      type: String
+    }
+  },
+  description: {
+    type: String
+  },
+  attendees: [{
+    type: String
+  }]
 }, {
-    collection: 'eventData'
-});
+  collection: 'eventData'
+})
 
 // create models from mongoose schemas
-const primarydata = mongoose.model('primaryData', primaryDataSchema);
-const eventdata = mongoose.model('eventData', eventDataSchema);
+const primarydata = mongoose.model('primaryData', primaryDataSchema)
+const eventdata = mongoose.model('eventData', eventDataSchema)
 
-// package the models in an object to export 
+// package the models in an object to export
 module.exports = { primarydata, eventdata }

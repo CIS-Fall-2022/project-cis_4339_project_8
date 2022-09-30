3de7e5d333ba5eef6268f32d2a00ec8251724239
epfarmer <epfarmer@cougarnet.uh.edu>
2022-09-29 21:44:29 -0500

    prettify backend

--- backend/models/models.js
+++ backend/models/models.js
@@ -3,118 +3,131 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 // collection for org
-const orgDataSchema = new Schema({
-  _id: {
-    type: String,
-    required: true
+const orgDataSchema = new Schema(
+  {
+    _id: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    }
   },
-  name: {
-    type: String,
-    required: true
+  {
+    collection: 'org'
   }
-}, {
-  collection: 'org'
-})
+)
 
 // collection for intakeData
-const clientDataSchema = new Schema({
-  _id: { type: String, default: uuid.v1 },
-  firstName: {
-    type: String,
-    required: true
-  },
-  middleName: {
-    type: String
-  },
-  lastName: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String
-  },
-  phoneNumber: {
-    primary: {
+const clientDataSchema = new Schema(
+  {
+    _id: { type: String, default: uuid.v1 },
+    firstName: {
       type: String,
       required: true
     },
-    alternate: {
-      type: String
-    }
-  },
-  address: {
-    line1: {
+    middleName: {
       type: String
     },
-    line2: {
-      type: String
-    },
-    city: {
+    lastName: {
       type: String,
       required: true
     },
-    county: {
+    email: {
       type: String
     },
-    zip: {
-      type: String
+    phoneNumber: {
+      primary: {
+        type: String,
+        required: true
+      },
+      alternate: {
+        type: String
+      }
+    },
+    address: {
+      line1: {
+        type: String
+      },
+      line2: {
+        type: String
+      },
+      city: {
+        type: String,
+        required: true
+      },
+      county: {
+        type: String
+      },
+      zip: {
+        type: String
+      }
+    },
+    orgs: {
+      type: [{ type: String, ref: 'org' }],
+      required: true,
+      validate: [(org) => org.length > 0, 'needs at least one org']
     }
   },
-  orgs: {
-    type: [{ type: String, ref: 'org' }],
-    required: true,
-    validate: [org => org.length > 0, 'needs at least one org']
+  {
+    collection: 'client',
+    timestamps: true
   }
-}, {
-  collection: 'client',
-  timestamps: true
-})
+)
 
 // collection for events
-const eventDataSchema = new Schema({
-  _id: { type: String, default: uuid.v1 },
-  org: {
-    type: String,
-    required: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  services: [{
-    type: String
-  }],
-  date: {
-    type: Date,
-    required: true
-  },
-  address: {
-    line1: {
+const eventDataSchema = new Schema(
+  {
+    _id: { type: String, default: uuid.v1 },
+    org: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    services: [
+      {
+        type: String
+      }
+    ],
+    date: {
+      type: Date,
+      required: true
+    },
+    address: {
+      line1: {
+        type: String
+      },
+      line2: {
+        type: String
+      },
+      city: {
+        type: String
+      },
+      county: {
+        type: String
+      },
+      zip: {
+        type: String
+      }
+    },
+    description: {
       type: String
     },
-    line2: {
-      type: String
-    },
-    city: {
-      type: String
-    },
-    county: {
-      type: String
-    },
-    zip: {
-      type: String
-    }
+    attendees: [
+      {
+        type: String,
+        ref: 'client'
+      }
+    ]
   },
-  description: {
-    type: String
-  },
-  attendees: [{
-    type: String,
-    ref: 'client'
-  }]
-}, {
-  collection: 'event'
-})
+  {
+    collection: 'event'
+  }
+)
 
 // create models from mongoose schemas
 const clients = mongoose.model('client', clientDataSchema)

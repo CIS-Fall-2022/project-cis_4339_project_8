82946535698934e1537ab0e0be6ca844be79f3d6
amolskiCIS <77849980+amolskiCIS@users.noreply.github.com>
2022-11-10 19:16:57 -0600

    Updated Delete Client Frontend
    More initial coding on deleting client on the frontend. Need to restructure it some more after a bit of reviewing my notes.

--- frontend/src/components/deleteClient.vue
+++ frontend/src/components/deleteClient.vue
@@ -23,26 +23,104 @@ export default {
     chosenClient(selectedClient) {
       this.client.firstName = selectedClient
     },
-    deleteClient(id){
-      const apiURL = import.meta.env.VITE_ROOT_API + '/clients/deleteClient/' + client._id;
-      let indexOfArrayItem = this.clients.findIndex((i) => i._id === id);
+    deleteClient(id) {
+      const apiURL =
+        import.meta.env.VITE_ROOT_API + '/clients/deleteClient/' + client._id
+      let indexOfArrayItem = this.clients.findIndex((i) => i._id === id)
 
       if (window.confirm('Do you really want to delete?')) {
-        axios.delete(apiURL).then(() => {
-            this.clients.splice(indexOfArrayItem, 1);
-          }).catch((error) => {
+        axios
+          .delete(apiURL)
+          .then(() => {
+            this.clients.splice(indexOfArrayItem, 1)
+          })
+          .catch((error) => {
             console.log(error)
-          });
+          })
       }
     }
   }
 }
 </script>
-
 <template>
-  <div class="row justify-content-center">
-    <div class="col-md-6">
-      <!-- Update Student content -->
+  <main>
+    <div>
+      <h1
+        class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
+      >
+        Delete Client
+      </h1>
     </div>
-  </div>
+    <div class="px-10 pt-20">
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
+      >
+        <h2 class="text-2xl font-bold">Delete Client By</h2>
+        <!-- Displays Client Name search field -->
+        <div class="flex flex-col">
+          <select
+            class="rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            v-model="searchBy"
+          >
+            <option value="Client Name">Client Name</option>
+            <option value="Client Number">Client Number</option>
+          </select>
+        </div>
+        <div class="flex flex-col" v-if="searchBy === 'Client Name'">
+          <label class="block">
+            <input
+              type="text"
+              class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              v-model="firstName"
+              v-on:keyup.enter="handleSubmitForm"
+              placeholder="Enter first name"
+            />
+          </label>
+        </div>
+        <div class="flex flex-col" v-if="searchBy === 'Client Name'">
+          <label class="block">
+            <input
+              type="text"
+              class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              v-model="lastName"
+              v-on:keyup.enter="handleSubmitForm"
+              placeholder="Enter last name"
+            />
+          </label>
+        </div>
+        <!-- Displays Client Number search field -->
+        <div class="flex flex-col" v-if="searchBy === 'Client Number'">
+          <input
+            class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            type="text"
+            v-model="phoneNumber"
+            v-on:keyup.enter="handleSubmitForm"
+            placeholder="Enter Client Phone Number"
+          />
+        </div>
+      </div>
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
+      >
+        <div></div>
+        <div></div>
+        <div class="mt-5 grid-cols-2">
+          <button
+            class="mr-10 border border-red-700 bg-white text-red-700 rounded"
+            @click="clearSearch"
+            type="submit"
+          >
+            Clear Search
+          </button>
+          <button
+            class="bg-red-700 text-white rounded"
+            @click="deleteClient"
+            type="submit"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>

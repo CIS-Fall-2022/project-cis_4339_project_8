08c0df0c022a667500066188313e9fea24db150e
amolskiCIS <77849980+amolskiCIS@users.noreply.github.com>
2022-11-10 19:03:35 -0600

    More deleteClient Code Beginnings
    Making more code into the deleteClient component, still in the beginning workings of it

--- frontend/src/components/deleteClient.vue
+++ frontend/src/components/deleteClient.vue
@@ -1,3 +1,48 @@
 <script>
+import axios from 'axios'
 
+export default {
+  data() {
+    return {
+      queryData: [],
+      // Parameter for search to occur
+      searchBy: '',
+      firstName: '',
+      lastName: '',
+      phoneNumber: ''
+    }
+  },
+  mounted() {
+    const apiURL = import.meta.env.VITE_ROOT_API + '/clients/'
+    axios.get(apiURL).then((res) => {
+      this.queryData = res.data
+    })
+    window.scrollTo(0, 0)
+  },
+  methods: {
+    chosenClient(selectedClient) {
+      this.client.firstName = selectedClient
+    },
+    deleteClient(id){
+      const apiURL = import.meta.env.VITE_ROOT_API + '/clients/deleteClient/' + client._id;
+      let indexOfArrayItem = this.clients.findIndex((i) => i._id === id);
+
+      if (window.confirm('Do you really want to delete?')) {
+        axios.delete(apiURL).then(() => {
+            this.clients.splice(indexOfArrayItem, 1);
+          }).catch((error) => {
+            console.log(error)
+          });
+      }
+    }
+  }
+}
 </script>
+
+<template>
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+      <!-- Update Student content -->
+    </div>
+  </div>
+</template>
--- frontend/src/components/deregister.vue
+++ frontend/src/components/deregister.vue
@@ -1,3 +1,19 @@
 <script>
-
+import useVuelidate from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+import axios from 'axios'
+    export default {
+        data() {
+            return {
+            }
+        }
+    }
 </script>
+
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <!-- Update Student content -->
+        </div>
+    </div>
+</template>

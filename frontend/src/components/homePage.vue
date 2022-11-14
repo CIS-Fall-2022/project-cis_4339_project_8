312c7b40527e1942456a724710e50dfc36f03085
epfarmer <epfarmer@cougarnet.uh.edu>
2022-11-13 20:41:20 -0600

    table

--- frontend/src/components/homePage.vue
+++ frontend/src/components/homePage.vue
@@ -71,54 +71,65 @@ export default {
 </script>
 
 <template>
-  <div>
-    <h1
-      class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
-    >
-      Welcome
-    </h1>
-  </div>
-  <section class="container">
-    <div class="columns">
-      <div class="column">
-        <div>
-          <AttendanceChart
-            v-if="!loading && !error"
-            :label="labels"
-            :chart-data="chartData"
-          ></AttendanceChart>
+  <main>
+    <div>
+      <h1
+        class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
+      >
+        Welcome
+      </h1>
+      <br />
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
+      >
+        <div class="ml-10"></div>
+        <div class="flex flex-col col-span-2">
+          <table class="min-w-full shadow-md rounded">
+            <thead class="bg-gray-50 text-xl">
+              <tr class="p-4 text-left">
+                <th class="p-4 text-left">Event Name</th>
+                <th class="p-4 text-left">Event Date</th>
+                <th class="p-4 text-left">Number of Attendees</th>
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-300">
+              <tr v-for="event in recentEvents" :key="event._id">
+                <td class="p-2 text-left">{{ event.name }}</td>
+                <td class="p-2 text-left">{{ formattedDate(event.date) }}</td>
+                <td class="p-2 text-left">{{ event.attendees.length }}</td>
+              </tr>
+            </tbody>
+          </table>
+          <div>
+            <AttendanceChart
+              v-if="!loading && !error"
+              :label="labels"
+              :chart-data="chartData"
+            ></AttendanceChart>
 
-          <!-- Start of loading animation -->
-          <div class="mt-40" v-if="loading">
-            <p
-              class="text-6xl font-bold text-center text-gray-500 animate-pulse"
-            >
-              Loading...
-            </p>
-          </div>
-          <!-- End of loading animation -->
+            <!-- Start of loading animation -->
+            <div class="mt-40" v-if="loading">
+              <p
+                class="text-6xl font-bold text-center text-gray-500 animate-pulse"
+              >
+                Loading...
+              </p>
+            </div>
+            <!-- End of loading animation -->
 
-          <!-- Start of error alert -->
-          <div class="mt-12 bg-red-50" v-if="error">
-            <h3 class="px-4 py-1 text-4xl font-bold text-white bg-red-800">
-              {{ error.title }}
-            </h3>
-            <p class="p-4 text-lg font-bold text-red-900">
-              {{ error.message }}
-            </p>
+            <!-- Start of error alert -->
+            <div class="mt-12 bg-red-50" v-if="error">
+              <h3 class="px-4 py-1 text-4xl font-bold text-white bg-red-800">
+                {{ error.title }}
+              </h3>
+              <p class="p-4 text-lg font-bold text-red-900">
+                {{ error.message }}
+              </p>
+            </div>
+            <!-- End of error alert -->
           </div>
-          <!-- End of error alert -->
-          <br />
-          <br />
         </div>
       </div>
     </div>
-  </section>
+  </main>
 </template>
-<style>
-#chart-wrapper {
-  display: inline-block;
-  position: relative;
-  width: 100%;
-}
-</style>

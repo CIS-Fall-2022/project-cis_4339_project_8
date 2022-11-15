0696b4cf845a8f8261cf342e430506559fcc757c
epfarmer <epfarmer@cougarnet.uh.edu>
2022-11-15 15:33:00 -0600

    added missing v-model connection

--- frontend/src/components/eventDetails.vue
+++ frontend/src/components/eventDetails.vue
@@ -144,6 +144,7 @@ export default {
           <div class="flex flex-col">
             <label class="block">
               <span class="text-gray-700">Description</span>
+              <!-- added missing v-model connection -->
               <textarea
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 rows="2"
